1521f07cdd8a6b696b12a4b93138b1f2558d97ab
Dario Berzano <dario.berzano@cern.ch>
2014-12-27 18:43:46 +0100

    Server: better PID file r/w in Daemon class

--- thing/pylib/piheat/daemon.py
+++ thing/pylib/piheat/daemon.py
@@ -10,18 +10,34 @@ import sys, os, time, atexit, signal
 class Daemon:
 
   def __init__(self, pidfile):
-    self.pidfile = pidfile
+    self._pidfile = pidfile
+    self.pid = None
+
+  ## Write pidfile.
+  def write_pid(self):
+    with open(self._pidfile, 'w') as pf:
+      pf.write( str(self.pid) + '\n' )
+
+  ## Read pidfile.
+  def read_pid(self):
+    try:
+      with open(self._pidfile, 'r') as pf:
+        self.pid = int( pf.read().strip() )
+    except (IOError, ValueError):
+      self.pid = None
 
   ## Daemonize class. Unix double-fork mechanism.
+  #
+  #  @return False on failure, True on success
   def daemonize(self):
 
     try:
       pid = os.fork()
       if pid > 0:
-        # exit first parent
+        # exit from first parent
         sys.exit(0)
-    except OSError as err:
-      sys.stderr.write('fork #1 failed: {0}\n'.format(err))
+    except OSError as e:
+      sys.stderr.write('Fork #1 failed: %s\n' % e)
       sys.exit(1)
 
     # decouple from parent environment
@@ -33,12 +49,14 @@ class Daemon:
     try:
       pid = os.fork()
       if pid > 0:
-
         # exit from second parent
         sys.exit(0)
-    except OSError as err:
-      sys.stderr.write('fork #2 failed: {0}\n'.format(err))
+    except OSError as e:
+      sys.stderr.write('Fork #2 failed: %s\n' % e)
       sys.exit(1)
+
+    # we are in the daemon: know our pid
+    self.pid = os.getpid()
 
     # redirect standard file descriptors
     sys.stdout.flush()
@@ -51,71 +69,51 @@ class Daemon:
     os.dup2(so.fileno(), sys.stdout.fileno())
     os.dup2(se.fileno(), sys.stderr.fileno())
 
-    # write pidfile
-    atexit.register(self.delpid)
+    # write pidfile and schedule deletion
+    atexit.register(self.del_pid)
+    self.write_pid()
 
-    pid = str(os.getpid())
-    with open(self.pidfile,'w+') as f:
-      f.write(pid + '\n')
-
-  def delpid(self):
-    os.remove(self.pidfile)
+  ## Delete pidfile.
+  def del_pid(self):
+    if os.path.isfile(self._pidfile):
+      os.remove(self._pidfile)
 
   ## Start the daemon.
   def start(self):
 
     # Check for a pidfile to see if the daemon already runs
-    try:
-      with open(self.pidfile,'r') as pf:
+    self.read_pid()
 
-        pid = int(pf.read().strip())
-    except IOError:
-      pid = None
-
-    if pid:
-      message = "pidfile {0} already exist. " + \
-          "Daemon already running?\n"
-      sys.stderr.write(message.format(self.pidfile))
+    if self.pid:
+      sys.stderr.write( 'PID file %s already exists: daemon already running?\n' % self._pidfile )
       sys.exit(1)
 
     # Start the daemon
     self.daemonize()
     self.run()
 
-  ## Stop the daemon
+  ## Stop the daemon.
   def stop(self):
 
     # Get the pid from the pidfile
-    try:
-      with open(self.pidfile,'r') as pf:
-        pid = int(pf.read().strip())
-    except IOError:
-      pid = None
+    self.read_pid()
 
-    if not pid:
-      message = "pidfile {0} does not exist. " + \
-          "Daemon not running?\n"
-      sys.stderr.write(message.format(self.pidfile))
+    if not self.pid:
+      sys.stderr.write( 'PID file %s does not exist: daemon not running?\n' % self._pidfile )
       return # not an error in a restart
 
     # Try killing the daemon process
     try:
-      while 1:
-        os.kill(pid, signal.SIGTERM)
+      while True:
+        os.kill(self.pid, signal.SIGTERM)
         time.sleep(0.1)
     except OSError as err:
       e = str(err.args)
       if e.find("No such process") > 0:
-        if os.path.exists(self.pidfile):
-          os.remove(self.pidfile)
+        self.del_pid()
       else:
         print (str(err.args))
         sys.exit(1)
-
-  ## Restart the daemon.
-  def restart(self):
-    self.stop()
-    self.start()
 
   ## You should override this method when you subclass Daemon.
   #
