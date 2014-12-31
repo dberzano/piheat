ff961c97a30775e49fcdeb200ea88d865320161e
Dario Berzano <dario.berzano@cern.ch>
2014-12-31 11:24:36 +0100

    Server: TimeStamp compliant with ISO format

--- thing/pylib/piheat/timestamp.py
+++ thing/pylib/piheat/timestamp.py
@@ -10,35 +10,61 @@ import calendar, datetime
 #  datetime and has methods with crystal clear names. Small but sufficient for our purposes.
 class TimeStamp:
 
-  # datefmt = Enum({
-  #   'NO_USEC': '%Y-%m-%d %H:%M:%S',
-  #   'DATE_ONLY': '%Y-%m-%d',
-  #   'TIME_ONLY': '%H:%M:%S'
-  # })
+  ## Predefined date-time output formats
+  format = {
+    'NO_USEC': '%Y-%m-%d %H:%M:%S',
+    'DATE_ONLY': '%Y-%m-%d',
+    'TIME_ONLY': '%H:%M:%S',
+    'ISO_FULL': '%Y-%m-%dT%H:%M:%S.%fZ'
+  }
+
 
   ## Initializes this `TimeStamp` instance from a UTC time stamp.
   #
   #  @param ts_utc A number with a timestamp treated as if it were in the UTC timezone, or `None` to
   #                initialize the object with current timestamp
+  #
+  #  @exception TypeError Raised if parameter is not a number
   def __init__(self, ts_utc=None):
     if ts_utc is None:
       self._dt_utc = datetime.datetime.utcnow()
     else:
       self._dt_utc = datetime.datetime.utcfromtimestamp(ts_utc)
 
+
   ## Initializes this `TimeStamp` from an ISO-formatted UTC date.
   #
+  #  This is an ISO-formatted UTC string:
+  #
+  #  ~~~{.txt}
+  #  2014-07-30T21:06:15.600000Z
+  #  ~~~
+  #
+  #  Note the `Z` that indicates the UTC timezone, and the `T` separating date from time.
+  #
+  #  In JavaScript, an ISO-formatted UTC string can be obtained from a `Date` object like this:
+  #
+  #  ~~~{.js}
+  #  var d = new Date();
+  #  console.log( d.toISOString() );
+  #  ~~~
+  #
   #  @param isodate A string with the ISO-formatted date in the UTC timezone
+  #
+  #  @exception ValueError Raised if string is malformed (not ISO-formatted UTC)
+  #  @exception TypeError  Raised if parameter is not a string
   @classmethod
   def from_iso_str(cls, isodate):
-    iso_to_dt = datetime.datetime.strptime(isodate, '%Y-%m-%dT%H:%M:%S.%fZ')
+    iso_to_dt = datetime.datetime.strptime(isodate, TimeStamp.format['ISO_FULL'])
     return cls( calendar.timegm( iso_to_dt.utctimetuple() ) + iso_to_dt.microsecond * 0.000001 )
+
 
   ## Gets current timestamp, including microseconds, in UTC.
   #
   #  @return A `Float` with the current timestamp in UTC
   def get_timestamp_usec_utc(self):
     return calendar.timegm( self._dt_utc.utctimetuple() ) + self._dt_utc.microsecond * 0.000001
+
 
   ## Gets a naive `datetime` object with this date in UTC.
   #
@@ -48,6 +74,7 @@ class TimeStamp:
   #  @return A naive `datetime` object
   def get_datetime_naive_utc(self):
     return self._dt_utc
+
 
   ## Gets a string with a formatted date. See
   #  [here](https://docs.python.org/2/library/time.html#time.strftime) for the format string
@@ -59,12 +86,15 @@ class TimeStamp:
   def get_formatted_str(self, format):
     return self._dt_utc.strftime(format)
 
-  ## String representation of a date.
+
+  ## ISO-formatted UTC string representation of the date.
   #
   #  @return A string representation of the current instance
+  #
+  #  @see from_iso_str
   def __str__(self):
-    #return self._dt_utc.strftime('%Y-%m-%e %H:%M:%S')
-    return str(self._dt_utc)
+    return self.get_formatted_str(TimeStamp.format['ISO_FULL'])
+
 
   ## Subtracts two `TimeStamp` objects.
   #
@@ -76,11 +106,39 @@ class TimeStamp:
   def __sub__(self, other):
     return datetime.timedelta(seconds=self.get_timestamp_usec_utc()-other.get_timestamp_usec_utc())
 
-  ## A simple unit test to test if this class works.
+
+  ## Check if functionalities provided by this class work.
   @staticmethod
   def assert_unit_test():
     tsraw = 1406754375.6
-    tsstr = '2014-07-30 21:06:15.600000'
-    tsobj = TimeStamp(1406754375.6)
-    assert tsobj.get_timestamp_usec_utc() == tsraw, 'UTC timestamps does not correspond'
-    assert str(tsobj) == tsstr, 'UTC string representations do not correspond'
+    tsstr = '2014-07-30T21:06:15.600000Z'
+    tsnou = '2014-07-30 21:06:15'
+
+    # constructing object from a UTC timestamp number
+    for v in [ tsraw, tsstr ]:
+
+      if isinstance(v, str):
+        label = 'From ISO string'
+        tsobj = TimeStamp.from_iso_str(v)
+      else:
+        label = 'From timestamp'
+        tsobj = TimeStamp(v)
+
+      assert tsobj.get_timestamp_usec_utc() == tsraw, \
+        '%s: UTC timestamps mismatch: %ld != %ld' % (label, tsobj.get_timestamp_usec_utc(), tsraw)
+      print '%s: timestamps test OK' % label
+
+      assert str(tsobj) == tsstr, \
+        '%s: UTC strings mismatch: %s != %s' % (label, str(tsobj), tsstr)
+      print '%s: strings test OK' % label
+
+      s = tsobj.get_formatted_str(TimeStamp.format['NO_USEC'])
+      assert s == tsnou, \
+        '%s: UTC strings without usec mismatch: %s != %s' % (s, tsnou)
+      print '%s: strings without usec test OK' % label
+
+    print 'All tests OK'
+
+
+if __name__ == '__main__':
+  TimeStamp.assert_unit_test()
