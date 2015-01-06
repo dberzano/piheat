c5f85ffeacdee94f1b802b3af1ffa321c6f6c35b
Dario Berzano <dario.berzano@cern.ch>
2015-01-06 09:32:57 +0100

    Web: status retrieval async again
    No longer necessary with f51383bf5e3d6c44836ff023c72556d45b034641
    
    Reverts 608a3c94588f6afb62c031bee8031a5468d1e978

--- web/js/misc.js
+++ web/js/misc.js
@@ -415,7 +415,7 @@
 
     read_status : function() {
 
-      $.ajax('https://dweet.io/get/dweets/for/' + cfg.thingid, { async: false })
+      $.get('https://dweet.io/get/dweets/for/' + cfg.thingid)
         .done(function(data) {
 
           var now = new Date();
