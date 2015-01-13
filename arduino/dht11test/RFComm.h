159300cc9a9f9519d611fd2067de9aab1d554418
Dario Berzano <dario.berzano@cern.ch>
2015-01-13 02:39:47 +0100

    Arduino: added more Doxygen comments

--- arduino/dht11test/RFComm.h
+++ arduino/dht11test/RFComm.h
@@ -10,22 +10,32 @@
 
 #include "Arduino.h"
 
+/// Protocol 1
 #define RFCPROTO_1 0
+/// Protocol 2
 #define RFCPROTO_2 1
+/// Protocol 3
 #define RFCPROTO_3 2
 
+/// Symbol for bit 0
 #define RFCSYM_0    0
+/// Symbol for bit 1
 #define RFCSYM_1    1
+/// Symbol for sync
 #define RFCSYM_SYNC 2
 
+/// A symbol: a sequence of variable length high and low signals.
+///
+/// Signal lengths are relative to a pulse length.
 typedef struct {
-  uint8_t hi;
-  uint8_t lo;
+  uint8_t hi;  ///< Relative duration of the high pulse
+  uint8_t lo;  ///< Relative duration of the low pulse
 } symbol_t;
 
+/// A protocol definition
 typedef struct {
-  unsigned int pulseLength_us;
-  symbol_t symbols[3];
+  unsigned int pulseLength_us;  ///< Pulse length, in µs
+  symbol_t symbols[3];          ///< Symbol shapes
 } proto_t;
 
 class RFComm {
