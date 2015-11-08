0adaf620ae5127c76e50b4d0e6c5b580e7f3ebd1
Dario Berzano <dario.berzano@cern.ch>
2015-11-08 13:21:48 +0100

    WIP: moved rfComm to new Arduino sketch

--- lowlevel/arduino_temprf/arduino_temprf.ino
+++ lowlevel/arduino_temprf/arduino_temprf.ino
@@ -3,9 +3,14 @@
 //         (e475755)
 
 #include "dht.h"
+#include "RFComm.h"
+
 #define DHT22_PIN 6
+#define RF433_PIN 9
+#define LED_PIN 13
 
 dht dht22;
+RFComm rf433 = RFComm(RF433_PIN, LED_PIN);
 
 const char *dht_err(char n) {
   static const char *dht_err_txt[6] = { "DHTLIB_OK",
@@ -28,12 +33,14 @@ const char *dht_err(char n) {
 
 void setup() {
   Serial.begin(115200);
+  rf433.setupSend(rfProtoV1, 3);
   Serial.println("arduino_temprf init");
 }
 
 void loop() {
   Serial.println(">> loop");
   int chk = dht22.read22(DHT22_PIN);
+  uint8_t rfdata[8];
   Serial.print("status: ");
   Serial.println(dht_err(chk));
   if (chk == DHTLIB_OK) {
@@ -42,6 +49,15 @@ void loop() {
     Serial.print("humidity: ");
     Serial.println(dht22.humidity);
   }
+  rfdata[0] = 1;
+  rfdata[1] = 2;
+  rfdata[2] = 3;
+  rfdata[3] = 4;
+  rfdata[4] = 5;
+  rfdata[5] = 6;
+  rfdata[6] = 7;
+  rfdata[7] = 8;
+  rf433.send(rfdata, 8);
   Serial.println("<< loop");
   delay(2000);
 }
