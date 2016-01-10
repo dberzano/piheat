7f95be336f178445a10928251fe9d5dc2f8cbeca
Dario Berzano <dario.berzano@cern.ch>
2016-01-11 00:05:04 +0100

    Upload to test path if not master branch

--- misc/lxplus.sh
+++ misc/lxplus.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -ex
 cd "$(dirname "$0")/.."
+DEST='www/ph'
+[[ "$(git rev-parse --abbrev-ref HEAD 2> /dev/null || true)" == master ]] || DEST='www/ph-test'
 rsync -av \
       --exclude '.*' \
       --exclude '.*/' \
       --exclude 'priv*' \
       --delete \
       --delete-excluded \
-      client/ dberzano@lxplus.cern.ch:www/ph/
+      client/ dberzano@lxplus.cern.ch:$DEST
 echo Sync to lxplus done
